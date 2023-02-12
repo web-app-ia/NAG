1c9131ed5554c37bb27f19851dcb6868e15b9b26
Rashmika Silva <silvarashmika98@gmail.com>
2023-02-12 12:50:56 +0530

    Addded discovery server with netflix eureka

--- services/avatar/src/main/java/org/ve/avatar/AvatarRunner.java
+++ services/avatar/src/main/java/org/ve/avatar/AvatarRunner.java
@@ -4,6 +4,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AvatarRunner {
     public static void main(String []args) throws IOException {
         ClassLoader classLoader = AvatarRunner.class.getClassLoader();
