924f254b950f8aaeb94a50c0014855307b4e3fb0
Rashmika Silva <silvarashmika98@gmail.com>
2023-02-15 15:48:05 +0530

    Added payment service as an eureka client to discovery server

--- services/payment/src/main/java/org/ve/payment/PaymentRunner.java
+++ services/payment/src/main/java/org/ve/payment/PaymentRunner.java
@@ -2,10 +2,12 @@ package org.ve.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentRunner {
     public static void main(String []args) {
         SpringApplication.run(PaymentRunner.class,args);
