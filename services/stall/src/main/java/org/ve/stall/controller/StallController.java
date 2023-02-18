cf7cf0e256d841d52946b562c55cc16773f204c1
Kavish-IR <kaveeshrajakaruna@gmail.com>
2023-02-18 09:32:20 +0530

    Added video uploading part and modified stall microservice

--- services/stall/src/main/java/org/ve/stall/controller/StallController.java
+++ services/stall/src/main/java/org/ve/stall/controller/StallController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 @RestController
 @RequiredArgsConstructor
@@ -19,18 +20,37 @@ public class StallController {
     @PostMapping("/api/stall/add")
     @ResponseStatus(HttpStatus.OK)
     public String addStall(@RequestBody Stall stall) throws InterruptedException, ExecutionException{
-        int stallId= stall.getStallId();
+        String stallId= stall.getStallId();
         return stallService.addStall(stall,stallId);
     }
-    @PostMapping("/api/stall/upload-logo")
+    @PutMapping("/api/stall/upload-logo")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> uploadLogo(@RequestParam("file") MultipartFile file) throws Exception {
-        return stallService.uploadLogo(file);
+    public ResponseEntity<String> uploadLogo(@RequestParam("file") MultipartFile file,@RequestParam String stallId) throws Exception {
+        return stallService.uploadLogo(file,stallId);
     }
-    @PostMapping("/api/stall/upload-banner")
+    @PutMapping("/api/stall/upload-banner")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> uploadBanner(@RequestParam("file") MultipartFile file) throws Exception {
-        return stallService.uploadBanner(file);
+    public ResponseEntity<String> uploadBanner(@RequestParam("file") MultipartFile file,@RequestParam String stallId) throws Exception {
+        return stallService.uploadBanner(file, stallId);
     }
+    @PutMapping("/api/stall/upload-video")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> uploadVideo(@RequestParam("file") MultipartFile file ,@RequestParam String stallId) throws Exception {
+        return stallService.uploadVideo(file, stallId);
+    }
+    @GetMapping ("/api/stall/get-stalls")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Stall> getStalls(@RequestParam String stallOwnerId) throws InterruptedException, ExecutionException{
+        return stallService.getStalls(stallOwnerId);}
+    @PutMapping ("/api/stall/update")
+    public String updateTicket(@RequestBody Stall stall, @RequestParam String stallId) throws InterruptedException, ExecutionException {
+        return stallService.updateStall(stall,stallId);
+    }
+    @DeleteMapping ("/api/stall/delete-stall")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteStall(@RequestParam String stallId) {
+        return stallService.deleteStall(stallId);
+    }
+
     }
 
--- services/stall/src/main/java/org/ve/stall/service/StallService.java
+++ services/stall/src/main/java/org/ve/stall/service/StallService.java
@@ -1,10 +1,8 @@
 package org.ve.stall.service;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.ve.stall.StallRunner;
 import org.ve.stall.model.Stall;
@@ -15,7 +13,6 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.json.simple.JSONObject;
 
@@ -23,26 +20,27 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+
 import static org.ve.stall.common.Constants.*;
 @Service
 public class StallService {
 
-    public String addStall(Stall stall, int stallId) throws InterruptedException, ExecutionException{
+    public String addStall(Stall stall, String stallId) throws InterruptedException, ExecutionException{
 
         Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionApiFuture = firestore.collection("stalls")
-                .document(Integer.toString(stallId)).set(stall);
+                .document(stallId).set(stall);
         return collectionApiFuture.get().getUpdateTime().toString();
     }
-    public ResponseEntity<String> uploadLogo(MultipartFile multipartFile) throws IOException {
+    public ResponseEntity<String> uploadLogo(MultipartFile multipartFile, String stallId) throws IOException {
         String objectName = generateFileName(multipartFile);
         ClassLoader classLoader = StallRunner.class.getClassLoader();
         File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
@@ -62,17 +60,16 @@ public class StallService {
         JSONObject obj=new JSONObject();
         obj.put("logoUrl",url);
         System.out.print(obj);
-        int stallId =1;
         Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionApiFuture = firestore.collection("stalls")
-                .document(Integer.toString(stallId)).update(obj);
+                .document(stallId).update(obj);
 
 
         return ResponseEntity.status(HttpStatus.CREATED).body("success");
 
     }
 
-    public ResponseEntity<String> uploadBanner(MultipartFile multipartFile) throws IOException {
+    public ResponseEntity<String> uploadBanner(MultipartFile multipartFile, String stallId) throws IOException {
         String objectName = generateFileName(multipartFile);
         ClassLoader classLoader = StallRunner.class.getClassLoader();
         File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
@@ -91,16 +88,56 @@ public class StallService {
         //imageUrl = String.format(DOWNLOAD_URL, URLEncoder.encode(objectName, StandardCharsets.UTF_8));
         JSONObject obj=new JSONObject();
         obj.put("bannerUrl",url);
-        int stallId =1;
         Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionApiFuture = firestore.collection("stalls")
-                .document(Integer.toString(stallId)).update(obj);
+                .document(stallId).update(obj);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("success");
+
+    }
+
+    public ResponseEntity<String> uploadVideo(MultipartFile multipartFile, String stallId) throws IOException {
+        String objectName = generateFileName(multipartFile);
+        ClassLoader classLoader = StallRunner.class.getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        File file2 = convertMultiPartToFile(multipartFile);
+        Path filePath = file2.toPath();
+
+        Storage storage = StorageOptions.newBuilder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).setProjectId(FIREBASE_PROJECT_ID).build().getService();
+        BlobId blobId = BlobId.of(FIREBASE_BUCKET, objectName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(multipartFile.getContentType()).build();
+        String url = "https://firebasestorage.googleapis.com/v0/b/"+FIREBASE_BUCKET+"/o/"+objectName+"?alt=media";
+
+        storage.create(blobInfo, Files.readAllBytes(filePath));
+        JSONObject obj=new JSONObject();
+        obj.put("videoUrl",url);
+        Firestore firestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionApiFuture = firestore.collection("stalls")
+                .document(stallId).update(obj);
 
 
         return ResponseEntity.status(HttpStatus.CREATED).body("success");
 
     }
 
+    public List<Stall> getStalls(String stallOwnerId) throws CancellationException, ExecutionException, InterruptedException {
+        Firestore firestore = FirestoreClient.getFirestore();
+        // asynchronously retrieve multiple documents
+        ApiFuture<QuerySnapshot> future = firestore.collection("stalls").whereEqualTo("stallOwnerId", stallOwnerId).get();
+// future.get() blocks on response
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        List<Stall> stalls = new ArrayList<>();
+        if (!documents.isEmpty()) {
+            documents.forEach((element) -> {
+                if (element.exists()) {
+                    stalls.add(element.toObject(Stall.class));
+                }
+            });
+            return stalls;
+        }
+        return null;
+    }
     private File convertMultiPartToFile(MultipartFile file) throws IOException {
         File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
         FileOutputStream fos = new FileOutputStream(convertedFile);
@@ -113,4 +150,23 @@ public class StallService {
         return new Date().getTime() + "-" + Objects.requireNonNull(multiPart.getOriginalFilename()).replace(" ", "_");
     }
 
+    public String deleteStall(String stallId) {
+        Firestore firestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionApiFuture = firestore.collection("stalls")
+                .document(stallId).delete();
+        return "Successfully deleted "+stallId;
+    }
+    public String updateStall(Stall stall, String stallId)throws InterruptedException, ExecutionException{
+        JSONObject obj=new JSONObject();
+        if(stall.getExhibitionId()!=null)
+        {obj.put("exhibitionId",stall.getExhibitionId());}
+        if(stall.getStallOwnerId()!=null)
+        {obj.put("stallOwnerId",stall.getStallOwnerId());}
+        if(stall.getStallColor()!=null)
+        {obj.put("stallColor",stall.getStallColor());}
+        Firestore firestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionApiFuture = firestore.collection("stalls")
+                .document(stallId).update(obj);
+        return "Updated "+collectionApiFuture.get().getUpdateTime().toString();
+    }
 }
